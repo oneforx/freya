4f0a80c67784e2ac772a11ec9c286f6c1c92e5bc
Hermann Vincent <baw.developpement@gmail.com>
2021-11-24 18:23:13 +0400

    fix postcss config

--- apps/imaeus-front/postcss.config.js
+++ apps/imaeus-front/postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: {
     tailwindcss: {
-      config: './apps/imaeurs-front/tailwind.config.js'
+      config: './apps/imaeus-front/tailwind.config.js'
     },
     autoprefixer: {},
   },
